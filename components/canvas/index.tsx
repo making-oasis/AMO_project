e3c40219cc66b97de66bfa4f9ac4e1fd19e5d98a
aladdin <hiromi_fukuda@voyagegroup.com>
2021-05-05 18:15:35 +0900

    edit

--- components/canvas/index.tsx
+++ components/canvas/index.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react"
-//import p5 from 'p5'
-import p5Types from "p5"
+import p5 from 'p5'
 
 
 const Canvas = (props: any) => {
     useEffect(() => {
-        new p5Types(props.sketch)
+        new p5(props.sketch)
 
     }, [props.sketch])
     return (<></>)
