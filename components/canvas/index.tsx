9204bac51c091ff26a886464d2084b456f03ca28
aladdin <hiromi_fukuda@voyagegroup.com>
2021-05-05 18:08:03 +0900

    p5.jsいれた

---
+++ components/canvas/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+import p5 from 'p5'
+//import p5Types from 'p5'
+
+
+const Canvas = (props: any) => {
+    useEffect(() => {
+        new p5(props.sketch)
+
+    }, [props.sketch])
+    return (<></>)
+}
+export default Canvas
