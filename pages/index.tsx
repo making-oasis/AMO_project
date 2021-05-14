f7ce01d8cb603d1304ef337e136fa483468fff38
aladdin <hiromi_fukuda@voyagegroup.com>
2021-05-14 19:27:55 +0900

    とちゅう

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,16 +11,18 @@ const IndexPage = () => {
       .scroll(() => {
         // selectors
         const $window = $(window);
-        const $body = $("#" + styles.container);
+        const $body = $("#" + styles.main);
         const $panel = $("#" + styles.panel);
+        console.log($window);
+        console.log(typeof $panel);
 
         const scroll = $window.scrollTop() + $window.height() / 3;
-        //console.log(scroll);
+        console.log(scroll);
 
         $panel.each(() => {
           const $this = $(this);
+          console.log(typeof $this);
           console.log("hello");
-          console.log($this.position().top);
 
           if (
             $this.position().top <= scroll &&
@@ -33,7 +35,7 @@ const IndexPage = () => {
 
             $body.addClass("color-" + $(this).data("color"));
           } else {
-            console.log("this");
+            console.log("here");
           }
         });
       })
@@ -41,51 +43,47 @@ const IndexPage = () => {
   }, []);
   return (
     <>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <div className={styles.panel} data-color="white">
-            <div>
-              <h1 className={styles.h1}>ここに説明書くよ</h1>
-              <p className={styles.p}>
-                あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-              </p>
-              <p>
-                あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-              </p>
-              <p>
-                あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-              </p>
-            </div>
-          </div>
-          <div className={styles.panel} data-color="violet">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="indigo">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="blue">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="green">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="yellow">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="orange">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="red">
-            <h2 className={styles.h2}>あああああああああ</h2>
-          </div>
-          <div className={styles.panel} data-color="red">
-            <div>
-              <p className={styles.description}>
-                <ButtonLink href="/top">Start</ButtonLink>
-              </p>
-            </div>
-          </div>
-        </main>
+      <div className={styles.panel} data-color="white">
+        <div>
+          <h1 className={styles.h1}>ここに説明書くよ</h1>
+          <p className={styles.p}>
+            あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+          </p>
+          <p>
+            あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+          </p>
+          <p>
+            あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+          </p>
+        </div>
+      </div>
+      <div className={styles.panel} data-color="violet">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="indigo">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="blue">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="green">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="yellow">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="orange">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="red">
+        <h2 className={styles.h2}>あああああああああ</h2>
+      </div>
+      <div className={styles.panel} data-color="red">
+        <div>
+          <p className={styles.description}>
+            <ButtonLink href="/top">Start</ButtonLink>
+          </p>
+        </div>
       </div>
     </>
   );
