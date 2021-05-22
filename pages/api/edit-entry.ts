d043adf3db32a3f73b7833ac14e8f426254d395b
tatsuya <myuthree1478@gmail.com>
2021-05-22 20:03:56 +0900

    edit-entry変更

--- pages/api/edit-entry.ts
+++ pages/api/edit-entry.ts
@@ -5,22 +5,22 @@ import { query } from "../../lib/db";
 const filter = new Filter();
 
 const handler: NextApiHandler = async (req, res) => {
-  const { id, title, content, report } = req.body;
+  const { id, toMessage, fromMessage, content, report } = req.body;
   try {
-    if (!id || !title || !content) {
+    if (!id || !toMessage || !fromMessage || !content) {
       return res
         .status(400)
-        .json({ message: "`id`,`title`, and `content` are all required" });
+        .json({ message: "`id`, `toMessage`, `fromMessage`, and `content` are all required" });
     }
 
     const results = await query(
       `
       UPDATE entries
-      SET title = ?, content = ?, report = ?
+      SET toMessage = ?, fromMessage = ?, content = ?, report = ?
       WHERE id = ?
       `,
-      //[filter.clean(title), filter.clean(content), filter.clean(report), id]
-      [title, content, report, id]
+      //[filter.clean(toMessage), filter.clean(fromMessage), filter.clean(content), filter.clean(report), id]
+      [toMessage, fromMessage, content, report, id]
     );
 
     return res.json(results);
