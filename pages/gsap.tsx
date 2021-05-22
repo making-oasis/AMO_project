5d361f3b3b43ceab61f575a8148103d64344e9cb
aladdin <hiromi_fukuda@voyagegroup.com>
2021-05-22 14:22:27 +0900

    冗長な背景作成

--- pages/gsap.tsx
+++ pages/gsap.tsx
@@ -1,5 +1,6 @@
 import { CSSTransition } from "react-transition-group";
 import { gsap } from "gsap";
+import * as React from "react";
 import Home from "../components/Gsap/home";
 
 const App = () => {
@@ -31,7 +32,45 @@ const App = () => {
       }
     );
   };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const cityBackground = React.useRef(null);
 
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const dallas = "hello";
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const austin = "hello";
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const newyork = "hello";
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const sanfrancisco = "hello";
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const beijing = "hello";
+
+  const cities = [
+    { name: "Dallas", image: dallas },
+    { name: "Austin", image: austin },
+    { name: "New York", image: newyork },
+    { name: "San Francisco", image: sanfrancisco },
+    { name: "Beijing", image: beijing },
+  ];
+
+  const test = (city, target) => {
+    console.log("ko");
+    gsap.to(target, {
+      duration: 0,
+      background: `url(${city}) center center`,
+    });
+    gsap.to(target, {
+      duration: 0.4,
+      opacity: 1,
+      ease: "power3.inOut",
+    });
+    gsap.from(target, {
+      duration: 0.4,
+      skewY: 2,
+      transformOrigin: "right top",
+    });
+  };
   return (
     <>
       <div className="container">
@@ -47,6 +86,18 @@ const App = () => {
             <Home />
           </div>
         </CSSTransition>
+      </div>
+      <div className="locations">
+        Locations:
+        {/* Returning the list of cities */}
+        {cities.map((el) => (
+          <span
+            key={el.name}
+            onMouseEnter={() => test(el.image, cityBackground)}
+          >
+            {el.name}
+          </span>
+        ))}
       </div>
     </>
   );
--- pages/top.tsx
+++ pages/top.tsx
@@ -3,16 +3,84 @@ import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import Container from "@/components/container";
 import EntryForm from "@/components/entry-form";
+//import { gsap } from "gsap";
+import Button from "@/components/button";
+//import Image from "next/image";
+
+import styles from "../styles/background.module.css";
 
 //投稿ページ(top page)
 const TopPage = () => {
+  const test = () => {
+    const ground = document.getElementById("ground");
+    ground.classList.remove(
+      styles.back1,
+      styles.back2,
+      styles.back3,
+      styles.back4,
+      styles.back5
+    );
+    ground.classList.add(styles.back2);
+  };
+  const test2 = () => {
+    const ground = document.getElementById("ground");
+    ground.classList.remove(
+      styles.back1,
+      styles.back2,
+      styles.back3,
+      styles.back4,
+      styles.back5
+    );
+    ground.classList.add(styles.back3);
+  };
+  const test3 = () => {
+    const ground = document.getElementById("ground");
+    ground.classList.remove(
+      styles.back1,
+      styles.back2,
+      styles.back3,
+      styles.back4,
+      styles.back5
+    );
+    ground.classList.add(styles.back3);
+  };
+  const test4 = () => {
+    const ground = document.getElementById("ground");
+    ground.classList.remove(
+      styles.back1,
+      styles.back2,
+      styles.back3,
+      styles.back4,
+      styles.back5
+    );
+    ground.classList.add(styles.back3);
+  };
+  const test5 = () => {
+    const ground = document.getElementById("ground");
+    ground.classList.remove(
+      styles.back1,
+      styles.back2,
+      styles.back3,
+      styles.back4,
+      styles.back5
+    );
+    ground.classList.add(styles.back5);
+  };
+
   return (
-    <div>
-      <Nav />
-      <Container>
-        <EntryForm />
-      </Container>
-      <Footer />
+    <div id="ground" className={styles.back1}>
+      <span>
+        <Nav />
+        <Container>
+          <EntryForm />
+          <Button onClick={test}>1</Button>
+          <Button onClick={test2}>2</Button>
+          <Button onClick={test3}>3</Button>
+          <Button onClick={test4}>4</Button>
+          <Button onClick={test5}>5</Button>
+        </Container>
+        <Footer />
+      </span>
     </div>
   );
 };
