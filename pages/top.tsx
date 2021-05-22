23843c624c127b78373fe1e14ea0c28e8fc25c4e
aladdin <hiromi_fukuda@voyagegroup.com>
2021-05-22 14:33:09 +0900

    追加

--- pages/top.tsx
+++ pages/top.tsx
@@ -9,57 +9,67 @@ import styles from "../styles/background.module.css";
 //投稿ページ(top page)
 const TopPage = () => {
   const test = () => {
+    console.log("test1");
     const ground = document.getElementById("ground");
     ground.classList.remove(
       styles.back1,
       styles.back2,
       styles.back3,
       styles.back4,
-      styles.back5
+      styles.back5,
+      styles.back6
     );
     ground.classList.add(styles.back2);
   };
   const test2 = () => {
+    console.log("test2");
     const ground = document.getElementById("ground");
     ground.classList.remove(
       styles.back1,
       styles.back2,
       styles.back3,
       styles.back4,
-      styles.back5
+      styles.back5,
+      styles.back6
     );
     ground.classList.add(styles.back3);
   };
   const test3 = () => {
+    console.log("test3");
     const ground = document.getElementById("ground");
     ground.classList.remove(
       styles.back1,
       styles.back2,
       styles.back3,
       styles.back4,
-      styles.back5
+      styles.back5,
+      styles.back6
     );
     ground.classList.add(styles.back4);
   };
   const test4 = () => {
+    console.log("test4");
     const ground = document.getElementById("ground");
     ground.classList.remove(
       styles.back1,
       styles.back2,
       styles.back3,
       styles.back4,
-      styles.back5
+      styles.back5,
+      styles.back6
     );
     ground.classList.add(styles.back5);
   };
   const test5 = () => {
+    console.log("test5");
     const ground = document.getElementById("ground");
     ground.classList.remove(
       styles.back1,
       styles.back2,
       styles.back3,
       styles.back4,
-      styles.back5
+      styles.back5,
+      styles.back6
     );
     ground.classList.add(styles.back6);
   };
