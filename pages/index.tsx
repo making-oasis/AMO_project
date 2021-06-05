696f36379e7e33ffa8846ad46db61d8e5c863d56
aladdin <hiromi_fukuda@voyagegroup.com>
2021-06-05 12:12:45 +0900

    途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,8 @@
 import ButtonLink from "@/components/button-link";
-import { useEffect } from "react";
-import $ from "jquery";
 import Image from "next/image";
 import styles from "../styles/opening.module.css";
 
-//urlクリック時のtopページanimation
 const IndexPage = () => {
-  useEffect(() => {
-    console.log("============== start DOM jQuery ==================");
-    $(window)
-      .scroll(() => {
-        // selectors
-        const $window = $(window);
-        const $body = $("#" + styles.main);
-        const $panel = $("#" + styles.panel);
-        console.log($window);
-        console.log(typeof $panel);
-
-        const scroll = $window.scrollTop() + $window.height() / 3;
-        console.log(scroll);
-
-        $panel.each(() => {
-          const $this = $(this);
-          console.log(typeof $this);
-          console.log("hello");
-
-          if (
-            $this.position().top <= scroll &&
-            $this.position().top + $this.height() > scroll
-          ) {
-            console.log("here");
-            $body.removeClass((index, css) => {
-              return (css.match(/(^|\s)color-\S+/g) || []).join(" ");
-            });
-
-            $body.addClass("color-" + $(this).data("color"));
-          } else {
-            console.log("here");
-          }
-        });
-      })
-      .scroll();
-  }, []);
-
   return (
     <>
       <div className={styles.panel}>
@@ -69,8 +29,7 @@ const IndexPage = () => {
           <h1 className={styles.h1}>
             小さなアクションから関心
             <br />
-            そして
-            <br />
+            <p className={styles.pp}>そして</p>
             より大きなアクションへ
           </h1>
           <p>
