f6ed02610a5d9e50a7d2f4f9991ae12764c37563
aladdin <hiromi_fukuda@voyagegroup.com>
2021-06-05 12:00:34 +0900

    途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import ButtonLink from "@/components/button-link";
 import { useEffect } from "react";
 import $ from "jquery";
+import Image from "next/image";
 import styles from "../styles/opening.module.css";
 
 //urlクリック時のtopページanimation
@@ -41,38 +42,56 @@ const IndexPage = () => {
       })
       .scroll();
   }, []);
+
   return (
     <>
-      <div className={styles.panel} data-color="white">
+      <div className={styles.panel}>
         <div>
-          <h1 className={styles.h1}>landing page</h1>
-          <p className={styles.p}>このWebAppが何であるかを文章ベースで表現</p>
+          <h1 className={styles.h1}>投稿を通して、小さなアクションの経験を</h1>
+          <p className={styles.p}>Experience small actions through posts</p>
         </div>
       </div>
-      <div className={styles.panel} data-color="violet">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
+      <div className={styles.panel}>
+        <div>
+          <p className={styles.p}>
+            {" "}
+            Aは難民選手団へ応援メッセージを送ることができるアプリです。
+            <br />{" "}
+            「メッセージを送る」という小さなアクションは難民支援に繋がります。
+          </p>
+          <p>
+            <Image src={"/landing/top.png"} width={500} height={400} />
+          </p>
+        </div>
       </div>
-      <div className={styles.panel} data-color="indigo">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
-      </div>
-      <div className={styles.panel} data-color="blue">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
-      </div>
-      <div className={styles.panel} data-color="green">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
-      </div>
-      <div className={styles.panel} data-color="yellow">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
-      </div>
-      <div className={styles.panel} data-color="orange">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
-      </div>
-      <div className={styles.panel} data-color="red">
-        <h2 className={styles.h2}>このWebAppが何であるかを文章ベースで表現</h2>
+      <div className={styles.panel}>
+        <div>
+          <h1 className={styles.h1}>
+            小さなアクションから関心
+            <br />
+            そして
+            <br />
+            より大きなアクションへ
+          </h1>
+          <p>
+            <Image src={"/landing/top2.png"} width={500} height={400} />
+          </p>
+          <p className={styles.p}>
+            {" "}
+            その積み重ねが大きな変化を生み出します。
+            <br /> そしてそのメッセージは実際に難民選手団へ届きます。
+          </p>
+        </div>
       </div>
       <div className={styles.panel} data-color="red">
         <div>
           <p className={styles.description}>
+            私たちは、この「メッセージを送る」というアクションを通して
+            <br />
+            より多くの人に難民問題に関心をもってもらえることを願っています。
+            <br />
+            メッセージを送ることで難民選手団を応援しましょう！
+            <br />
             <ButtonLink href="/top">Start</ButtonLink>
           </p>
         </div>
