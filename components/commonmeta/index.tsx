4b4b3ba735740354ed9dbd3427dbc166c426adb7
aladdin <hiromi_fukuda@voyagegroup.com>
2021-07-12 22:43:27 +0900

    meta

--- components/commonmeta/index.tsx
+++ components/commonmeta/index.tsx
@@ -6,17 +6,31 @@ type Props = {
 };
 
 const CommonMeta: React.FunctionComponent<Props> = ({ title, description }) => {
+  const url = "fly-your-message.com/";
+  const imgUrl = "https://og-image.vercel.app/hello_world.png";
+  const imgWidth = 300;
+  const imgHeight = 300;
+
   return (
     <Head>
       <title>{title}</title>
-      <meta property="description" content={description} />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta name="description" content={description} />
+      <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
       <meta property="og:description" content={description} />
-      <meta
-        property="og:image"
-        content={`${process.env.SITE_URL}/ogp_large.png`}
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={imgUrl} />
+      <meta property="og:image:width" content={String(imgWidth)} />
+      <meta property="og:image:height" content={String(imgHeight)} />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;display=swap"
+        rel="stylesheet"
       />
-      <meta name="twitter:card" content="summary_large_image" />
+      <link rel="canonical" href={url} />
+      <image></image>
     </Head>
   );
 };
