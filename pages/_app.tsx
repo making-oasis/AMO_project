bfc153b2be2347d8990ee2ac1cdbf1d67e9cda38
aladdin <hiromi_fukuda@voyagegroup.com>
2021-07-15 23:04:26 +0900

    feat:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/index.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>fly-your-message</title>
+        <meta property="og:title" content="fly-your-message" />
+        <meta
+          property="og:description"
+          content="このWebアプリは、難民の方々に応援メッセージを届けることが出来るアプリです。
+難民の方にメッセージをを届けることが出来るWebアプリを作りました。"
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
